20e80ac318a4c414a1f576a496581792d17449dd
Ayub <ayyub.amini@gmail.com>
2022-07-14 17:23:29 -0400

    Add files via upload

---
+++ AspNet.Security.OAuth.Okta/LoggingExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AspNet.Security.OAuth.Okta
+{
+    internal static partial class LoggingExtensions
+    {
+        [LoggerMessage(1, LogLevel.Debug, "HandleChallenge with Location: {Location}; and Set-Cookie: {Cookie}.", EventName = "HandleChallenge")]
+        public static partial void HandleChallenge(this ILogger logger, string location, string cookie);
+    }
+}
