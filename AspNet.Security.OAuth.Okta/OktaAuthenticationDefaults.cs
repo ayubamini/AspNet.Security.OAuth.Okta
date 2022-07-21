e1c30773012b4a435ea2d1571f12e29f0d801946
Ayub <ayyub.amini@gmail.com>
2022-07-21 12:36:43 -0400

    Update OktaAuthenticationDefaults.cs

--- AspNet.Security.OAuth.Okta/OktaAuthenticationDefaults.cs
+++ AspNet.Security.OAuth.Okta/OktaAuthenticationDefaults.cs
@@ -8,14 +8,14 @@
 
         public static readonly string DisplayName = "Okta";
 
-        public static readonly string Issuer = "https://dev-26827217.okta.com/oauth2/oktaserver";
+        public static readonly string Issuer = "https://dev-xxx.okta.com/oauth2/oktaserver";
 
         public static readonly string CallbackPath = "/redirect";
 
-        public static readonly string AuthorizationEndpoint = "https://dev-26827217.okta.com/oauth2/oktaserver/v1/authorize";
+        public static readonly string AuthorizationEndpoint = "https://dev-xxx.okta.com/oauth2/oktaserver/v1/authorize";
 
-        public static readonly string TokenEndpoint = "https://dev-26827217.okta.com/oauth2/oktaserver/v1/token";
+        public static readonly string TokenEndpoint = "https://dev-xxx.okta.com/oauth2/oktaserver/v1/token";
 
-        public static readonly string UserInformationEndpoint = "https://dev-26827217.okta.com/oauth2/oktaserver/v1/userinfo";
+        public static readonly string UserInformationEndpoint = "https://dev-xxx.okta.com/oauth2/oktaserver/v1/userinfo";
     }
 }
